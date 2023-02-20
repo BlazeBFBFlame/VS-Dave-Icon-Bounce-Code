6d943e88192a32a50c73df91d47a631c963343e5
BlazeBFBFlame <105085698+BlazeBFBFlame@users.noreply.github.com>
2023-02-19 20:30:17 -0700

    Add files via upload

---
+++ replace line 2993 - 3004 with this.hx
@@ -0,0 +1,12 @@
+iconP1.setGraphicSize(Std.int(FlxMath.lerp(150, iconP1.width, 0.8)),Std.int(FlxMath.lerp(150, iconP1.height, 0.8)));
+		iconP2.setGraphicSize(Std.int(FlxMath.lerp(150, iconP2.width, 0.8)),Std.int(FlxMath.lerp(150, iconP2.height, 0.8)));
+
+		iconP1.updateHitbox();
+		iconP2.updateHitbox();
+
+		var iconOffset:Int = 26;
+
+		iconP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) - iconOffset);
+		iconP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01)) - (iconP2.width - iconOffset);
+
+        //REPLACE 2993 - 3004 IN PLAYSTATE WITH THE CODE  ABOVE!!!!!!!!!
